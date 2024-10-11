4bd6ef0eafd99a7ceb71cb4303c773c228774ae4
joche3 <75068346@certus.edu.pe>
2024-10-10 19:45:23 -0500

    Agrego la estrucutra base del Smartcontract ERC20 para un Crowdfunding

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.SEPOLIA_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY, // Your Etherscan API key
+  },
+
+};
